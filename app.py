c7137267df7694e0f563125f63a46ba954485bf1
PoalChaynik <alpolaop13@gmail.com>
2022-11-14 11:54:50 +0200

    11.14

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def datums():
 def lietotajs(vards,vecums):
     # return f'<h1>{vards}, {vecums}</h1>'
     with open('data.json','w',encoding='UTF-8') as file:
-        
+        return
 
 app.run(host='0.0.0.0',port=81)
