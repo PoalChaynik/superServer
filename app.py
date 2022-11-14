7fd6b441d5d488b637c52c9f0460d7d950f57f8f
PoalChaynik <alpolaop13@gmail.com>
2022-11-14 11:52:46 +0200

    14.11

---
+++ app.py
@@ -0,0 +1,28 @@
+#try_flask
+
+
+from flask import Flask
+import json
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'nothing is here'
+
+@app.route('/hello')
+def hello():
+    return '<h1>Hello, World</h1>'
+
+@app.route('/date')
+def datums():
+    return (f'<h1>Today is {datetime.datetime.now()}</h1>')
+
+@app.route('/lietotajs/<vards>/<vecums>')
+def lietotajs(vards,vecums):
+    # return f'<h1>{vards}, {vecums}</h1>'
+    with open('data.json','w',encoding='UTF-8') as file:
+        
+
+app.run(host='0.0.0.0',port=81)
