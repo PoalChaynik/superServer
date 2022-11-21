3812c5a99aacf1ec521df43d4e9e37a1655a6cce
PoalChaynik <alpolaop13@gmail.com>
2022-11-21 11:59:57 +0200

    21/11

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import datetime
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"]=False
 
 @app.route('/')
 def index():
@@ -15,17 +16,43 @@ def index():
 def hello():
     return '<h1>Hello, World</h1>'
 
+@app.route('/send/<vards>/<zina>')
+def send(vards,zina):
+    now = datetime.datetime.now()
+    time = now.strftime("%Y/%m/%d, %H:%M:%S")
+
+    # print(vards,zina,time)
+
+    rinda = {
+        'zina':zina,
+        'vards':vards,
+        'laiks':time
+    }
+
+    with open('chat.json','r',encoding='utf-8') as fails:
+        vecasZinas = fails.read()
+        vecieJson = json.loads(vecasZinas)
+
+    vecieJson.append(rinda)
+
+    with open('chat.json','w',encoding='utf-8') as f:
+        json.dump(vecieJson,f,indent=2,ensure_ascii=False)
+
+    return 'ok'
+
 @app.route('/date')
 def datums():
     return (f'<h1>Today is {datetime.datetime.now()}</h1>')
 
-@app.route('/lietotajs/<vards>/<vecums>')
-def lietotajs(vards,vecums):
-    # return f'<h1>{vards}, {vecums}</h1>'
-    with open('data.json','w',encoding='UTF-8') as file:
-        dict = {'vards': vards,
-                'vecums': vecums}
 
-        json.dumps(dict)
+
+@app.route('/user/<vards>/<vecums>')
+def lietotajs(vards,vecums):
+    #return f"{vards} un {vecums}"
+    dati = {vards:vecums}
+    dati_json = json.dumps(dati)
+    #return dati_json
+    with open("lietotaji.json","w",encoding="utf-8") as fails:
+        json.dump(dati_json,fails,indent=2,ensure_ascii=False)
 
 app.run(host='0.0.0.0',port=81)
